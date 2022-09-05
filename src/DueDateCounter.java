fec86ada79fdaa507a4a90e770cbf979ad91e206
Gallelee <thimotus@gmail.com>
2022-09-05 16:48:37 -0400

    Add java program for counting days until lab due dates

---
+++ src/DueDateCounter.java
@@ -0,0 +1,21 @@
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+
+public class DueDateCounter {
+    public static LocalDate lab1Date = LocalDate.of(2022,9,27);
+    public static LocalDate lab2Date = LocalDate.of(2022,10,26);
+    public static LocalDate lab3Date = LocalDate.of(2022,11,23);
+    public static LocalDate lab4Date = LocalDate.of(2022,12,7);
+
+
+    public static void main(String[] args){
+        LocalDate date = LocalDate.now();
+        System.out.println("There are " + date.until(lab1Date, ChronoUnit.DAYS) + " days until lab 1 is due");
+        System.out.println("There are " + date.until(lab2Date, ChronoUnit.DAYS) + " days until lab 2 is due");
+        System.out.println("There are " + date.until(lab3Date, ChronoUnit.DAYS) + " days until lab 3 is due");
+        System.out.println("There are " + date.until(lab4Date, ChronoUnit.DAYS) + " days until lab 4 is due");
+
+
+    }
+}
