3fa70b4ea7171a2fd66e35de91db9b6655ce3d9d
Gallelee <thimotus@gmail.com>
2022-10-17 08:11:21 -0400

    Add reading in of user input to choose preselected dates or own date

--- Lab1/src/DueDateCounter.java
+++ Lab1/src/DueDateCounter.java
@@ -1,5 +1,6 @@
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.Scanner;
 
 
 public class DueDateCounter {
@@ -11,10 +12,20 @@ public class DueDateCounter {
 
     public static void main(String[] args){
         LocalDate date = LocalDate.now();
-        System.out.println("There are " + date.until(lab1Date, ChronoUnit.DAYS) + " day(s) until lab 1 is due");
-        System.out.println("There are " + date.until(lab2Date, ChronoUnit.DAYS) + " day(s) until lab 2 is due");
-        System.out.println("There are " + date.until(lab3Date, ChronoUnit.DAYS) + " day(s) until lab 3 is due");
-        System.out.println("There are " + date.until(lab4Date, ChronoUnit.DAYS) + " day(s) until lab 4 is due");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Would you like to see days until all due dates[insert Y], or select your own date[insert N]");
+        while(scan.hasNext()){
+            if(scan.next().equals("Y")){
+                System.out.println("There are " + date.until(lab1Date, ChronoUnit.DAYS) + " day(s) until lab 1 is due");
+                System.out.println("There are " + date.until(lab2Date, ChronoUnit.DAYS) + " day(s) until lab 2 is due");
+                System.out.println("There are " + date.until(lab3Date, ChronoUnit.DAYS) + " day(s) until lab 3 is due");
+                System.out.println("There are " + date.until(lab4Date, ChronoUnit.DAYS) + " day(s) until lab 4 is due");
+            }
+            else if(scan.next().equals("N")){
+                System.out.println("Enter your date in");
+            }
+        }
+        
 
 
     }
