1be5820013e9d22f97e078acd43cd1df3ded3b00
Gallelee <thimotus@gmail.com>
2022-10-24 15:21:02 -0400

    Call build functions from sample.sh

--- Lab1/src/sample.sh
+++ Lab1/src/sample.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-source ./build.sh
 
-echo "calling the runProgram function from build.sh via sample.sh: runProgram is a function from build.sh that calls twoother functions"
-runProgram
+echo "calling the runProgram function from build.sh via sample.sh: runProgram is a function from build.sh that calls two other functions"
+ source build.sh
 
 
