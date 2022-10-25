9142a6eb755a89e9a338b6ba58c703ac212a1bcc
Gallelee <thimotus@gmail.com>
2022-10-25 03:18:06 -0400

    Add makefile

--- Lab1/src/sample.sh
+++ Lab1/src/sample.sh
@@ -2,7 +2,7 @@
 
 
 echo "calling the runProgram function from build.sh via sample.sh: runProgram is a function from build.sh that calls two other functions"
-print $((date --version))
+date --version
 source build.sh
 
 
